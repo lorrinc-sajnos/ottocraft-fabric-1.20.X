b3d798ce2351d6b251b3f71b58dc67bd6b047bb6
Ottó Lőrinc <olorinc2003@gmail.com>
2024-02-19 01:33:20 +0100

    Initial commit

---
+++ src/main/java/com/lorrinc_sajnos/ottocraft/OttocraftClient.java
@@ -0,0 +1,10 @@
+package com.lorrinc_sajnos.ottocraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OttocraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/lorrinc_sajnos/ottocraft/OttocraftDataGenerator.java
@@ -0,0 +1,11 @@
+package com.lorrinc_sajnos.ottocraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class OttocraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
